555da0d892b07ea0d342c3bac26abe2543f69557
Murat Çakmak <muratcakmak5252@gmail.com>
2021-10-17 21:30:19 +0300

    new empty db added

---
+++ spotbot.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 17 Eki 2021, 20:28:42
+-- Sunucu sürümü: 10.4.17-MariaDB
+-- PHP Sürümü: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `spotbot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `coins`
+--
+
+CREATE TABLE `coins` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `profit` decimal(10,5) DEFAULT NULL,
+  `purchase` decimal(10,5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Tablo döküm verisi `coins`
+--
+
+INSERT INTO `coins` (`id`, `name`, `profit`, `purchase`) VALUES
+(1, 'ADA', '0.00800', '0.00400'),
+(2, 'TRX', '0.00800', '0.00400'),
+(3, 'DOGE', '0.00010', '0.00400'),
+(4, 'VET', '0.00800', '0.00400'),
+(5, 'XRP', '0.00800', '0.00400'),
+(6, 'MATIC', '0.00700', '0.00400');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` bigint(20) NOT NULL,
+  `type` tinyint(4) NOT NULL,
+  `coin_id` int(11) DEFAULT NULL,
+  `title` text DEFAULT NULL,
+  `description` text NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` bigint(11) NOT NULL,
+  `coin_id` int(11) NOT NULL,
+  `orderId` bigint(20) NOT NULL,
+  `symbol` varchar(255) NOT NULL,
+  `side` varchar(255) NOT NULL,
+  `origQty` decimal(13,8) NOT NULL DEFAULT 0.00000000,
+  `price` decimal(13,8) NOT NULL DEFAULT 0.00000000,
+  `type` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `fee` decimal(13,8) NOT NULL DEFAULT 0.00000000,
+  `total` decimal(13,8) NOT NULL DEFAULT 0.00000000,
+  `var_piece` decimal(13,8) DEFAULT 0.00000000,
+  `var_price` decimal(13,8) DEFAULT 0.00000000,
+  `json_data` text DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `order_logs`
+--
+
+CREATE TABLE `order_logs` (
+  `id` bigint(20) NOT NULL,
+  `unique_id` varchar(255) DEFAULT NULL,
+  `orderId` bigint(20) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `coins`
+--
+ALTER TABLE `coins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `order_logs`
+--
+ALTER TABLE `order_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `coins`
+--
+ALTER TABLE `coins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=831;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `order_logs`
+--
+ALTER TABLE `order_logs`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1584;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
